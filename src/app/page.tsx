372943bac71a804840cb6961ec601e84dc3495a4
Ronnakrit888 <ronnakrit.t@kkumail.com>
2024-06-20 00:24:14 +0700

    update table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,118 @@
-  'use client'
+"use client";
 
-  import React, { useState } from "react";
-  import { Container, Button } from "@mui/material";
-  import styles from './page.module.css'
-  import EmptyBox from "./shared/components/box-subject";
+import React, { useState } from "react";
+import { Container, Button, Typography } from "@mui/material";
+import styles from "./page.module.css";
 
-  type GridType = string[][]
+export default function Home() {
+  const days: string[] = ["MON", "TUE", "WED", "THU", "FRI"];
+  const times: string[] = [
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+  ];
 
-  export default function Home() {
-
-    const days : string[] = ["MON", "TUE", "WED", "THU", "FRI"]
-    const times : string[] = ['8', '9', '10', '11', '12']
-
-    const initialGrid : GridType = days.map(() => Array(times.length * 2).fill(""))
-    const [grid, setGrid] = useState<GridType>(initialGrid)
-
-    return (
-      <Container sx={{ 
-        maxWidth: 2000,
-        maxHeight: 1000 
-        }}>
-        <table> 
-          <thead>
-            <tr>
-              <th style={{ width: 100 }}>Day/Time</th>
-              {times.map((time, timeIndex) => (
-                <th key={timeIndex} className={styles.timeHeader} colSpan={4}>{time}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody> 
-          {days.map((dayname, dayIndex) => (
-            <tr key={dayIndex}>
-              <th className={styles.dayHeader}>{dayname}</th>
-              {grid[dayIndex].map((cell, cellIndex) => (
-                <th key={cellIndex} className={styles.subject} colSpan={2}></th>
-              ))}
-            </tr>
+  return (
+    <Container sx={{ maxWidth: "1440px" }}>
+      <div style={{ display: "block" }}>
+        <div style={{ width: "100%" }}>
+          <div
+            className={styles.dayHeader}
+            style={{
+              top: "0px",
+              left: "0px",
+              width: "98px",
+              height: "58px",
+              backgroundColor: "grey",
+            }}
+          >
+            <Typography variant="h6" color="black">
+              Day/Time
+            </Typography>
+          </div>
+          {days.map((value, index) => (
+            <div
+              className={styles.dayHeader}
+              style={{
+                top: `${61 + index * 81}px`,
+                left: "0px",
+                width: "98px",
+                height: "78px",
+              }}
+            >
+              <Typography variant="h6" color="black">
+                {value}
+              </Typography>
+            </div>
           ))}
-          </tbody>
-        </table>
-
-        <Button variant="outlined">Add EmptyBox</Button>
-      </Container>
-    );
-  }
-
+          {times.map((value, index) => (
+            <div
+              key={index}
+              className={styles.dayHeader}
+              style={{
+                top: "0px",
+                left: `${101 + index * 101}px`,
+                width: "98px",
+                height: "58px",
+              }}
+            >
+              {value}
+            </div>
+          ))}
+        </div>
+      </div>
+      <span
+        className={styles.line}
+        style={{
+          top: "-0.5px",
+          left: "-0.5px",
+          width: "1414px",
+          height: "1px",
+        }}
+      ></span>
+      {[...Array(days.length + 1)].map((_, index) => (
+        <span
+          key={index}
+          className={styles.line}
+          style={{
+            top: `${60 + index * 81}px`,
+            left: "-0.5px",
+            width: "1414px",
+            height: "1px",
+          }}
+        ></span>
+      ))}
+      <span
+        className={styles.line}
+        style={{
+          top: "-0.5px",
+          left: "-0.5px",
+          width: "1px",
+          height: "1000px",
+        }}
+      ></span>
+      {[...Array(times.length + 1)].map((_, index) => (
+        <span
+          key={index}
+          className={styles.line}
+          style={{
+            top: "-0.5px",
+            left: `${100 + index * 101}px`,
+            width: "1px",
+            height: "466px",
+          }}
+        ></span>
+      ))}
+    </Container>
+  );
+}
