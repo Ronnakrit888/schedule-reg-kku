c53d183814fa25ca8bb29821f89f64d3c815f7a7
Ronnakrit888 <ronnakrit.t@kkumail.com>
2024-06-20 00:41:31 +0700

    update table again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,96 +23,100 @@ export default function Home() {
   ];
 
   return (
-    <Container sx={{ maxWidth: "1440px" }}>
-      <div style={{ display: "block" }}>
-        <div style={{ width: "100%" }}>
-          <div
-            className={styles.dayHeader}
-            style={{
-              top: "0px",
-              left: "0px",
-              width: "98px",
-              height: "58px",
-              backgroundColor: "grey",
-            }}
-          >
-            <Typography variant="h6" color="black">
-              Day/Time
-            </Typography>
-          </div>
-          {days.map((value, index) => (
+    <Container>
+      <Container sx={{ maxWidth: "1440px" }}>
+        <div style={{ display: "block" }}>
+          <div style={{ width: "100%" }}>
             <div
-              className={styles.dayHeader}
-              style={{
-                top: `${61 + index * 81}px`,
-                left: "0px",
-                width: "98px",
-                height: "78px",
-              }}
-            >
-              <Typography variant="h6" color="black">
-                {value}
-              </Typography>
-            </div>
-          ))}
-          {times.map((value, index) => (
-            <div
-              key={index}
               className={styles.dayHeader}
               style={{
                 top: "0px",
-                left: `${101 + index * 101}px`,
+                left: "0px",
                 width: "98px",
                 height: "58px",
+                backgroundColor: "grey",
               }}
             >
-              {value}
+              <Typography variant="h6" color="black">
+                Day/Time
+              </Typography>
             </div>
-          ))}
+            {days.map((value, index) => (
+              <div
+                className={styles.dayHeader}
+                style={{
+                  top: `${61 + index * 81}px`,
+                  left: "0px",
+                  width: "98px",
+                  height: "78px",
+                }}
+              >
+                <Typography variant="h6" color="black">
+                  {value}
+                </Typography>
+              </div>
+            ))}
+            {times.map((value, index) => (
+              <div
+                key={index}
+                className={styles.dayHeader}
+                style={{
+                  top: "0px",
+                  left: `${101 + index * 101}px`,
+                  width: "98px",
+                  height: "58px",
+                }}
+              >
+                <Typography variant="h6" color="initial">
+                  {value}
+                </Typography>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <span
-        className={styles.line}
-        style={{
-          top: "-0.5px",
-          left: "-0.5px",
-          width: "1414px",
-          height: "1px",
-        }}
-      ></span>
-      {[...Array(days.length + 1)].map((_, index) => (
         <span
-          key={index}
           className={styles.line}
           style={{
-            top: `${60 + index * 81}px`,
+            top: "-0.5px",
             left: "-0.5px",
             width: "1414px",
             height: "1px",
           }}
         ></span>
-      ))}
-      <span
-        className={styles.line}
-        style={{
-          top: "-0.5px",
-          left: "-0.5px",
-          width: "1px",
-          height: "1000px",
-        }}
-      ></span>
-      {[...Array(times.length + 1)].map((_, index) => (
+        {[...Array(days.length + 1)].map((_, index) => (
+          <span
+            key={index}
+            className={styles.line}
+            style={{
+              top: `${60 + index * 81}px`,
+              left: "-0.5px",
+              width: "1414px",
+              height: "1px",
+            }}
+          ></span>
+        ))}
         <span
-          key={index}
           className={styles.line}
           style={{
             top: "-0.5px",
-            left: `${100 + index * 101}px`,
+            left: "-0.5px",
             width: "1px",
-            height: "466px",
+            height: "1000px",
           }}
         ></span>
-      ))}
+        {[...Array(times.length + 1)].map((_, index) => (
+          <span
+            key={index}
+            className={styles.line}
+            style={{
+              top: "-0.5px",
+              left: `${100 + index * 101}px`,
+              width: "1px",
+              height: "466px",
+            }}
+          ></span>
+        ))}
+      </Container>
     </Container>
   );
 }
