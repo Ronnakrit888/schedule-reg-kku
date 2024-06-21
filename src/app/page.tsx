6a1e19be5ee72007d55fe514ba5195dfebc3fc6e
Ronnakrit888 <ronnakrit.t@kkumail.com>
2024-06-21 11:59:19 +0700

    before edit again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,13 @@
 import React, { useState } from "react";
 import { Container, Button, Typography } from "@mui/material";
 import styles from "./page.module.css";
+import BoxSubject from "./shared/components/box-subject";
 
 export default function Home() {
+
+  const [isClick, setClick] = useState<boolean>(false)
+  const [positionLeft, setPositionLeft] = useState<number>(0)
+  const [positionTop, setPositionTop] = useState<number>(0)
 
   const days: string[] = ["MON", "TUE", "WED", "THU", "FRI"];
   const times: string[] = [
@@ -23,67 +28,73 @@ export default function Home() {
     "20",
   ];
 
-  const day : string = "MON"
-  const timeStart : number = 9
-  const timeEnd : number = 10.30
+  const day: string = "MON";
+  const timeStart: number = 8;
+  const timeEnd: number = 10.30;
 
-  const getDecimalPart = (num : number) : number => {
-    
-    const integerPart = Math.floor(num)
-    const decimalPart = num - integerPart
-    return Math.floor(decimalPart * 100)
+  const getDecimalPart = (num: number): number => {
+    const integerPart = Math.floor(num);
+    const decimalPart = num - integerPart;
+    return Math.floor(decimalPart * 100);
+  };
 
-  }
+  const lengthSubject = (start: number, end: number): number => {
+    const startInteger: number = Math.floor(start);
+    const endInteger: number = Math.floor(end);
 
-  const lengthSubject = (start : number, end : number) : number => {
-
-    const startInteger : number = Math.floor(start)
-    const endInteger : number = Math.floor(end)
-
-    let minutes : number = 0
+    let minutes: number = 0;
 
     if (!Number.isInteger(start) && Number.isInteger(end)) {
-      console.log("Time Start is not Integer")
-      minutes = getDecimalPart(start) - 60
+      console.log("Time Start is not Integer");
+      minutes = getDecimalPart(start) - 60;
     }
 
     if (Number.isInteger(start) && !Number.isInteger(end)) {
-      console.log("Time End is not Integer")
-      minutes = 60 - getDecimalPart(end)
+      console.log("Time End is not Integer");
+      minutes = 60 - getDecimalPart(end);
     }
 
     if (!Number.isInteger(start) && !Number.isInteger(end)) {
-      console.log("Both is not Integer")
-      minutes = getDecimalPart(end) - getDecimalPart(start)
+      console.log("Both is not Integer");
+      minutes = getDecimalPart(end) - getDecimalPart(start);
     }
 
-    return (endInteger - startInteger) * 2 + (minutes / 30)
-
-  }
-
+    return (endInteger - startInteger) * 2 + minutes / 30;
+  };
 
   const addSubject = () => {
-    const tableArray : number[][] = Array(days.length + 1).fill(null).map((_, index) => new Array(times.length * 2).fill(0))
-    console.log(tableArray)
 
-    const dayIndex = days.indexOf(day)
-    console.log("Day Index : " , dayIndex + 1)
-    console.log("length : ", lengthSubject(timeStart, timeEnd))
+    setClick(false)
+    const tableArray: number[][] = Array.from({ length: days.length + 1 }, () =>
+      Array(times.length * 2).fill(0)
+    );
 
-    const distanceLeft : number = lengthSubject(parseInt(times[0]), timeStart)
-    console.log("from left :", distanceLeft)
+    console.log(tableArray);
 
-   for (let i = 0; i < lengthSubject(timeStart, timeEnd); i++) {
-    tableArray[dayIndex + 1][distanceLeft + i] = 1
-   }
-    console.log(tableArray)
+    const dayIndex = days.indexOf(day);
+    setPositionTop(dayIndex + 1)
+    console.log("Day Index : ", dayIndex + 1);
+    console.log("length : ", lengthSubject(timeStart, timeEnd));
 
-  }
-  
+    const distanceLeft: number = lengthSubject(parseInt(times[0]), timeStart);
+    setPositionLeft(distanceLeft)
+    console.log("from left :", distanceLeft);
+
+    const newTableArray: number[][] = tableArray.map((row) => [...row]);
+
+    for (let i = 0; i < lengthSubject(timeStart, timeEnd); i++) {
+      newTableArray[dayIndex + 1][distanceLeft + i] = 1;
+    }
+
+    console.log(newTableArray);
+  };
+
   return (
-    <Container>
-      <Container sx={{ maxWidth: "1440px" }}>
-        <div style={{ display: "block" }}>
+
+    <>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Container maxWidth='lg' style={{ width : '100%', marginLeft : 'auto', boxSizing : 'border-box', marginRight : 'auto', display : 'block'}} />
+        <div style={{ position : 'relative' }}>
           <div style={{ width: "100%" }}>
             <div
               className={styles.dayHeader}
@@ -93,6 +104,7 @@ export default function Home() {
                 width: "98px",
                 height: "58px",
                 backgroundColor: "grey",
+                zIndex: 0,
               }}
             >
               <Typography variant="h6" color="black">
@@ -100,83 +112,95 @@ export default function Home() {
               </Typography>
             </div>
             {days.map((value, index) => (
-              <div
-                className={styles.dayHeader}
-                style={{
-                  top: `${61 + index * 81}px`,
-                  left: "0px",
-                  width: "98px",
-                  height: "78px",
-                }}
-              >
-                <Typography variant="h6" color="black">
-                  {value}
-                </Typography>
-              </div>
-            ))}
-            {times.map((value, index) => (
-              <div
-                key={index}
-                className={styles.dayHeader}
-                style={{
-                  top: "0px",
-                  left: `${101 + index * 101}px`,
-                  width: "98px",
-                  height: "58px",
-                }}
-              >
-                <Typography variant="h6" color="initial">
-                  {value}
-                </Typography>
-              </div>
-            ))}
-          </div>
+            <div
+              key={index}
+              className={styles.dayHeader}
+              style={{
+                top: `${61 + index * 81}px`,
+                left: "0px",
+                width: "98px",
+                height: "78px",
+                zIndex: 0,
+              }}
+            >
+              <Typography variant="h6" color="black">
+                {value}
+              </Typography>
+            </div>
+          ))}
+          {times.map((value, index) => (
+            <div
+              key={index}
+              className={styles.dayHeader}
+              style={{
+                top: "0px",
+                left: `${101 + index * 101}px`,
+                width: "98px",
+                height: "58px",
+                zIndex: 0,
+              }}
+            >
+              <Typography variant="h6" color="initial">
+                {value}
+              </Typography>
+            </div>
+          ))}
         </div>
+      </div>
+      <span
+        className={styles.line}
+        style={{
+          top: "-1px",
+          left: "-1px",
+          width: "1414px",
+          height: "1px",
+          zIndex: 0,
+        }}
+      ></span>
+      {[...Array(days.length + 1)].map((_, index) => (
         <span
+          key={index}
           className={styles.line}
           style={{
-            top: "-0.5px",
-            left: "-0.5px",
+            top: `${60 + (index * 81)}px`,
+            left: "0px",
             width: "1414px",
             height: "1px",
+            zIndex: 0,
           }}
         ></span>
-        {[...Array(days.length + 1)].map((_, index) => (
-          <span
-            key={index}
-            className={styles.line}
-            style={{
-              top: `${60 + index * 81}px`,
-              left: "-0.5px",
-              width: "1414px",
-              height: "1px",
-            }}
-          ></span>
-        ))}
+      ))}
+      <span
+        className={styles.line}
+        style={{
+          top: "-1px",
+          left: "-1px",
+          width: "1px",
+          height: "466px",
+          zIndex: 0,
+        }}
+      ></span>
+      {[...Array(times.length + 1)].map((_, index) => (
         <span
+          key={index}
           className={styles.line}
           style={{
-            top: "-0.5px",
-            left: "-0.5px",
+            top: "-1px",
+            left: `${100 + index * 101}px`,
             width: "1px",
-            height: "1000px",
+            height: "466px",
+            zIndex: 0,
           }}
         ></span>
-        {[...Array(times.length + 1)].map((_, index) => (
-          <span
-            key={index}
-            className={styles.line}
-            style={{
-              top: "-0.5px",
-              left: `${100 + index * 101}px`,
-              width: "1px",
-              height: "466px",
-            }}
-          ></span>
-        ))}
-      </Container>
-      <Button variant="outlined" sx={{ top : '500px'}} onClick={addSubject}>Mock</Button>
-    </Container>
-    
+      ))}
+
+      {isClick && (
+        <BoxSubject fromLeft={positionLeft} fromTop={positionTop} length={lengthSubject(timeStart, timeEnd)} />
+      )}
+    </div><Button variant="outlined" sx={{ top: "500px" }} onClick={addSubject}>
+        Mock
+      </Button><Button variant="outlined" sx={{ top: "500px" }} onClick={() => setClick(true)}>
+        Click
+      </Button><div /></>
   );
 }
--- src/app/shared/components/box-subject.tsx
+++ src/app/shared/components/box-subject.tsx
@@ -2,26 +2,29 @@ import React from 'react'
 import { Box } from '@mui/material'
 
 interface BoxProps {
-  row : number,
-  column : number,
+  fromLeft : number,
+  fromTop : number,
+  length : number
 
 }
 
-const BoxSubject : React.FC<BoxProps>= ({row, column}) => {
+const BoxSubject : React.FC<BoxProps>= ({fromLeft, fromTop, length}) => {
+
+
   return (
-    <Box
-        sx={{
-            width : 75,
-            height : 75,
-            borderRadius : 1,
-            bgcolor : '#007FFF',
-            ' &:hover' : {
-                bgcolor : '#0066CC'
-            },
-            border: '2px solid grey'
-                       
-        }}
-    />
+    <div
+      style={{
+        top: `${61 + ((fromTop - 1) * 80)}px`,
+        left: `${102 + (fromLeft * 50)}px`,
+        width: `${50 * length}px`,
+        height: "78px",
+        backgroundColor: "rgb(75, 112, 245)",
+        zIndex: 10,
+        position: "absolute",
+        borderRadius : '3px',
+        border : '1px solid rgb(76, 59, 207)'
+      }}
+    ></div>
   )
 }
 
